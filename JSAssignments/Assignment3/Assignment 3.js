8b758b844ceda870dc7b2ac2a5a810fbc1896f7c
Nicholas_Domingue <nad018@email.latech.edu>
2023-02-06 10:53:13 -0600

    assignment 3 edit

--- JSAssignments/Assignment3/Assignment 3.js
+++ JSAssignments/Assignment3/Assignment 3.js
@@ -1,87 +1,117 @@
-//class for all sprite objects that will be animated
-class Character {
-  //the paremeter for each sprite generated is the spritesheet.
-  constructor(spritesheet) {
-    //load animation, the position, and which direction the sprite is facing.
-    this.animation = loadAnimation(spritesheet, {frameSize: [80, 80], frames: 6});
-    this.x = 0;
-    this.y = 0;
-    this.flipped = false;
-  }
-  
-  //this will initialize each character at a random location on the screen one time per instance.
-  display() {
-    if (!this.initialized) {
-      this.x = random(0, 800);
-      this.y = random(0, 400);
-      this.initialized = true;
-    }
-    
-    //when the character is moving to the left, the scale function will flip the character to face the left.
-    push();
-    if (this.flipped) {
-      scale(-1, 1);
-      animation(this.animation, -this.x, this.y);
-    } else {
-      animation(this.animation, this.x, this.y);
-    }
-    pop();    
-  }
+
+//create variables for spritesheets and array for all class objects
+let spriteSheet1;
+let spriteSheet2;
+
+let walkingAnimations = [];
+
+//this function just gets a random number 
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-//make an array for all sprite objects that will be generated through the Character class.
-//set keyIsPressed to false by default.
-let characters = [];
-let keyIsPressed = false;
+//preload the spritesheets
+function preload() {
+  spriteSheet1 = loadImage("Eskimo.png");
+  spriteSheet2 = loadImage("Ninja.png");
+}
 
-//create canvas and initialize any characters.
+//setup canvas and create 3 objects in WalkingAnimation class and push them into an array
 function setup() {
-  createCanvas(displayWidth, displayHeight);
-  characters.push(new Character('Eskimo.png'));
-  characters.push(new Character('Ninja.png'));
-  characters.push(new Character('Eskimo.png'));
+  createCanvas(400, 400);
+  imageMode(CENTER);
+
+  walkingAnimations.push(new WalkingAnimation(spriteSheet1,80,80,getRandomNumber(0,400),getRandomNumber(0,400),9));
+  walkingAnimations.push(new WalkingAnimation(spriteSheet1,80,80,getRandomNumber(0,400),getRandomNumber(0,400),9));
+  walkingAnimations.push(new WalkingAnimation(spriteSheet2,80,80,getRandomNumber(0,400),getRandomNumber(0,400),9));
 }
 
-//draw out each character in the array list of characters.
+//draw all objects in array
 function draw() {
-  clear();
-  background('lightgray');
-  characters.forEach(c => { c.display() });
-
-  //if the key is pressed, move the characters x postion by 5 pixels to left or right.
-  //if left key is pressed, change flip value to true to flipp character.
-  if (keyIsPressed) {
-    if (keyCode === LEFT_ARROW) {
-      characters.forEach(c => {
-        // doesnt allow sprite to leave screen
-        if (c.x > 0) {
-          c.x -= 5;
-        }
-        c.flipped = true;
-      });
-    } else if (keyCode === RIGHT_ARROW) {
-      characters.forEach(c => {
-        //doesnt allow sprite to leave screen
-        if (c.x < width - 80) {
-          c.x += 5;
-        }
-        c.flipped = false;
-      });
-    }
+  background(220);
+  
+  for (let i = 0; i < walkingAnimations.length; i++) {
+    walkingAnimations[i].draw();
+    walkingAnimations[i].checkBounds();
   }
 }
 
-//if the userer is pressing on a key, change the default value of keyIsPressed to true, 
-//so that in the draw function it will update its position.
+//functions for when key is pressed and not pressed which effects the position of all objects.
 function keyPressed() {
-  keyIsPressed = true;
+  for (let i = 0; i < walkingAnimations.length; i++) {
+    walkingAnimations[i].keyPressed(RIGHT_ARROW, LEFT_ARROW);
+  }
 }
 
 function keyReleased() {
-  keyIsPressed = false;
+  for (let i = 0; i < walkingAnimations.length; i++) {
+    walkingAnimations[i].keyReleased(RIGHT_ARROW, LEFT_ARROW);
+  }
 }
 
+// WalkingAnimation class that takes spritesheet, size, position, and animation length as parameters.
+class WalkingAnimation {
+  constructor(spritesheet, sw, sh, dx, dy, animationLength, offsetX = 0, offsetY = 0) {
+    this.spritesheet = spritesheet;
+    this.sw = sw;
+    this.sh = sh;
+    this.dx = dx;
+    this.dy = dy;
+    this.u = 0;
+    this.v = 0;
+    this.animationLength = animationLength;
+    this.currentFrame = 0;
+    this.moving = 0;
+    this.xDirection = 1;
+    this.offsetX = offsetX;
+    this.offsetY = offsetY;
+  }
+
+  //draw out the animation from the spritesheet.
+  draw() {
+    this.u = (this.moving != 0) ? this.currentFrame % this.animationLength : 0;
+    push();
+    translate(this.dx, this.dy);
+    scale(this.xDirection, 1);
+
+    image(this.spritesheet, 0, 0, this.sw, this.sh, this.u * this.sw + this.offsetX, this.v * this.sh + this.offsetY, this.sw, this.sh);
+    pop();
+    if (frameCount % 6 == 0) {
+      this.currentFrame++;
+    }
+
+    this.dx += this.moving;
+    this.checkBounds();
+  }
+
+  //move object to the left and right, if moving left, this.moving one changes the scale to -1
+  keyPressed(right, left) {
+    if (keyCode === right) {
+      this.moving = 1;
+      this.xDirection = 1;
+      this.currentFrame = 1;
+    } else if (keyCode === left) {
+      this.moving = -1;
+      this.xDirection = -1;
+      this.currentFrame = 1;
+    }
+  }
+
+  //if the key is released, object will stop moving.
+  keyReleased(right, left) {
+    if (keyCode === right || keyCode === left) {
+      this.moving = 0;
+    }
+  }
 
 
-
+// if sprite moves off canvas it will appear on the opposite side of the canvas
+  checkBounds() {
+    if (this.dx > width) {
+      this.dx = 0;
+    } else if (this.dx < 0) {
+      this.dx = width;
+    }
+  }
+}
 
